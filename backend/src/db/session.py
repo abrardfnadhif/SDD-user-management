6a5015e7b8109141a44f8d25741a3278d64aea67
abrardfnadhif <abrar.d.f.nadhif@gdplabs.id>
2025-10-04 16:11:13 +0700

    fix: allow extra fields in Settings to ignore unused .env variables
    Added 'extra = "ignore"' to Settings.Config to allow .env.sample
    to contain additional configuration fields that aren't used yet.
    
    This prevents validation errors when .env has fields like:
    - jwt_secret_key
    - rate_limit_*
    - email_*
    - etc.
    
    These will be added to Settings class as needed when implementing
    those features.

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -26,6 +26,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         case_sensitive = False
+        extra = "ignore"  # Ignore extra fields in .env file
 
 
 # Create settings instance
